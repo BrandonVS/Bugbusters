7d8298b549209c512498edee09992be8c073edd7
BrandonVS <49170851+BrandonVS@users.noreply.github.com>
2022-07-15 20:38:35 -0500

    GestorOleadas

--- Bugbusters/Assets/Scripts/WaveSpawner.cs
+++ Bugbusters/Assets/Scripts/WaveSpawner.cs
@@ -1,28 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
     public Transform bugPrefab;
+    public Transform inicio;
+    public Text oleadaN;
 
     public float intermedio = 3f;
     private float countdown = 3f;
+    private int oleada = 1;
+
+    private Transform bug;
 
     void Update()
     {
         if (countdown <= 0f)
         {
-            SpawnWave();
+            float intervaloBugs = Random.Range(0.2f, 0.7f);
+            int numeroBugs = Random.Range(2, 4);
+            StartCoroutine(ApareceBug(numeroBugs, intervaloBugs));
             countdown = intermedio;
         }
 
         countdown -= Time.deltaTime;
+
+
+        oleadaN.text = "Oleada: " + oleada.ToString();
     }
 
-    // Update is called once per frame
-    void SpawnWave()
+    IEnumerator ApareceBug(int numeroBugs, float intervaloBugs)
     {
-        Debug.Log("The bugs are comming");
+
+        for (int i = 0; i < numeroBugs; i++)
+        {
+            bug = Instantiate(bugPrefab, inicio.position, inicio.rotation);
+
+            yield return new WaitForSeconds(0.2f);
+        }
+        oleada++;
     }
 }
